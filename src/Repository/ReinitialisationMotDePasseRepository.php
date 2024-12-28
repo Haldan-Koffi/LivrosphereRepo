58969c1a66c11da4906f6dd67b326da69b79b44a
Haldan Koffi <e.haldankoffi@gmail.com>
2024-12-28 13:51:33 +0100

    implementation du processus de reinitialisation de mot de passe

---
+++ src/Repository/ReinitialisationMotDePasseRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\ReinitialisationMotDePasse;
+use App\Entity\Utilisateur;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestInterface;
+use SymfonyCasts\Bundle\ResetPassword\Persistence\Repository\ResetPasswordRequestRepositoryTrait;
+use SymfonyCasts\Bundle\ResetPassword\Persistence\ResetPasswordRequestRepositoryInterface;
+
+/**
+ * @extends ServiceEntityRepository<ResetPasswordRequest>
+ */
+class ReinitialisationMotDePasseRepository extends ServiceEntityRepository implements ResetPasswordRequestRepositoryInterface
+{
+    use ResetPasswordRequestRepositoryTrait;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ReinitialisationMotDePasse::class);
+    }
+
+    /**
+     * @param Utilisateur $user
+     */
+    public function createResetPasswordRequest(object $user, \DateTimeInterface $expiresAt, string $selector, string $hashedToken): ResetPasswordRequestInterface
+    {
+        return new ReinitialisationMotDePasse($user, $expiresAt, $selector, $hashedToken);
+    }
+}
